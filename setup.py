b237a505d07ce4c3f4fd0cc363b546126568fd05
Arun <arun@siara.cc>
2020-09-06 18:02:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sqlite_micro_logger_py',
+    version='0.0.1',
+    description='Logger for Sqlite with low memory footprint',
+    license='MIT',
+    packages=['sqlite_micro_logger'],
+    author='Arundale Ramanathan',
+    author_email='arun@siara.cc',
+    keywords=['sqlite database logger'],
+    url='https://github.com/siara-cc/sqlite_micro_logger_py'
+)
